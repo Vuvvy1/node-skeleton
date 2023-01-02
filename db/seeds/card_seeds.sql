f92359155e23229c14626e9bdfdc924218733b52
Hadley <sutherlandhadley@gmail.com>
2023-01-02 12:32:05 -0700

    add sample card db

---
+++ db/seeds/card_seeds.sql
@@ -0,0 +1,4 @@
+ INSERT INTO cards (
+    admin_id, title,  admin_id, thumbnail_photo_url, cost, active)
+    VALUES (
+    'Gliscor','https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH8/SWSH8_EN_141.png', true);
