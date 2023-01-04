809b7a76349ad47f6dfce0a8f66a5709717ce6e0
Hadley <sutherlandhadley@gmail.com>
2023-01-03 17:18:43 -0700

    add more seeds

--- db/seeds/card_seeds.sql
+++ db/seeds/card_seeds.sql
@@ -8,3 +8,28 @@
     title, thumbnail_photo_url, cost, active)
     VALUES (
     'Ferrothorn','https://assets.pokemon.com/assets/cms2/img/cards/web/SM9/SM9_EN_103.png', 400, true);
+
+ INSERT INTO cards (
+    title, thumbnail_photo_url, cost, active)
+    VALUES (
+    'Ferroseed','https://assets.pokemon.com/assets/cms2/img/cards/web/SWSH4/SWSH4_EN_123.png', 350, true);
+
+ INSERT INTO cards (
+    title, thumbnail_photo_url, cost, active)
+    VALUES (
+    'Snivy','https://assets.pokemon.com/assets/cms2/img/cards/web/BWP/BWP_EN_BW01.png', 200, false);
+
+--  INSERT INTO cards (
+--     title, thumbnail_photo_url, cost, active)
+--     VALUES (
+--     'Ferrothorn','https://assets.pokemon.com/assets/cms2/img/cards/web/SM9/SM9_EN_103.png', 400, true);
+
+--  INSERT INTO cards (
+--     title, thumbnail_photo_url, cost, active)
+--     VALUES (
+--     'Ferrothorn','https://assets.pokemon.com/assets/cms2/img/cards/web/SM9/SM9_EN_103.png', 400, true);
+
+--  INSERT INTO cards (
+--     title, thumbnail_photo_url, cost, active)
+--     VALUES (
+--     'Ferrothorn','https://assets.pokemon.com/assets/cms2/img/cards/web/SM9/SM9_EN_103.png', 400, true);
